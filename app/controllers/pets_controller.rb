ed8cf8f73feed245c4c4895a40ae12b325268540
ricky martinez <rickyjmartinez2@gmail.com>
2023-08-15 15:08:30 -0400

    add edit/update action to pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,4 +8,33 @@ class PetsController < ApplicationController
     @pet = Pet.find_by(id: params[:id])
     render :show
   end
+
+  def new
+    @pet = Pet.new
+    render :new
+  end
+
+  def create
+    @pet = Pet.create(
+      name: params[:pet][:name],
+      breed: params[:pet][:breed],
+      image: params[:pet][:image],
+    )
+    redirect_to "/pets"
+  end
+
+  def edit
+    @pet = Pet.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @pet = Pet.find_by(id: params[:id])
+    @pet.update(
+      name: params[:pet][:name],
+      breed: params[:pet][:breed],
+      image: params[:pet][:image],
+    )
+    redirect_to "/pets"
+  end
 end
