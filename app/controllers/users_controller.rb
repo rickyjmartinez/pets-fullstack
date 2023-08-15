38599d41063981fedd57d15f6705cf4d79ebe9a4
ricky martinez <rickyjmartinez2@gmail.com>
2023-08-15 16:00:50 -0400

    add redirect_to paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     )
     if @user.save
       session[:user_id] = @user.id
-      redirect_to "/"
+      redirect_to "/pets"
     else
       render :new, status: :unprocessable_entity
     end
