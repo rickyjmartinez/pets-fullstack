6069f4356638c93e20b520b63d7d756f3cb9ce28
ricky martinez <rickyjmartinez2@gmail.com>
2023-08-15 15:19:43 -0400

    add new and create actions in users controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :pets
+
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
 end
